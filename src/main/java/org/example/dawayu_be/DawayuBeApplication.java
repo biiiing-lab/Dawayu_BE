64547cce99d9c24668c23654ab3b9189583cb430
jangy <jangyb0901@gmail.com>
2024-07-19 11:35:12 +0900

    build : 개발환경 설정

---
+++ src/main/java/org/example/dawayu_be/DawayuBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.dawayu_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawayuBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawayuBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dawayu_be/DawayuBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dawayu_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawayuBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
