e7650f5e1337060b63b7cf3ba47901da4bf4eeb3
jangy <jangyb0901@gmail.com>
2024-07-19 11:57:32 +0900

    feat : User Entity 생성

---
+++ src/main/java/org/example/dawayu_be/users/UsersEntity.java
@@ -0,0 +1,30 @@
+package org.example.dawayu_be.users;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Table(name = "users")
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsersEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userNo;
+
+    private String userId;
+    private String password;
+    private String nickName;
+    private String email;
+
+    @CreationTimestamp
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm")
+    private LocalDateTime register;
+}
