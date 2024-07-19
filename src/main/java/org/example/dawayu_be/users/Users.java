38b1cf019e55790cc7699c17d86bbc14aa73d1f7
jangy <jangyb0901@gmail.com>
2024-07-19 12:03:22 +0900

    fix : User Entity class file name 수정

--- src/main/java/org/example/dawayu_be/users/Users.java
+++ src/main/java/org/example/dawayu_be/users/Users.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class UsersEntity {
+public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userNo;
