b18900fa4355957ab51177f901de1e4c122e9b50
jangy <jangyb0901@gmail.com>
2024-07-22 02:40:04 +0900

    fix : 권한 처리
    - 권한 있는 자만 게시글 등록 가능
    - 권한 있거나 없거나 모두 게시글 세부 내용 조회 가능

--- src/main/java/org/example/dawayu_be/config/WebSecurityConfig.java
+++ src/main/java/org/example/dawayu_be/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.example.dawayu_be.jwt.JwtAuthenticationFilter;
 import org.example.dawayu_be.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,6 +34,7 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(
                         authorization -> authorization.requestMatchers("/auth/login").permitAll() // 로그인 경로 허용
                                 .requestMatchers("/auth/signup").permitAll() // 회원가입 경로 허용
+                                .requestMatchers(HttpMethod.GET, "/posts/{articleNo}").permitAll()
                                 .anyRequest().authenticated())
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
