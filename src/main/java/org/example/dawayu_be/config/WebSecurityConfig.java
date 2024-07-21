f54f989f1b6690ec552db1debe08f8fb48d9ecb6
jangy <jangyb0901@gmail.com>
2024-07-21 22:06:34 +0900

    fix : webSecuriy config 수정 - http basic - session - formLogin -> all disable

--- src/main/java/org/example/dawayu_be/config/WebSecurityConfig.java
+++ src/main/java/org/example/dawayu_be/config/WebSecurityConfig.java
@@ -1,29 +1,45 @@
 package org.example.dawayu_be.config;
 
 import lombok.RequiredArgsConstructor;
+import org.example.dawayu_be.jwt.JwtAuthenticationFilter;
+import org.example.dawayu_be.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig  {
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final JwtTokenProvider tokenProvider;
+    private final CorsConfig corsConfig;
+
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity, CorsConfig corsConfig) throws Exception {
+        return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         authorization -> authorization.requestMatchers("/auth/login").permitAll() // 로그인 경로 허용
                                 .requestMatchers("/auth/signup").permitAll() // 회원가입 경로 허용
-                                .anyRequest().authenticated()
-                );
-        return httpSecurity.build();
+                                .anyRequest().authenticated())
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new JwtAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
+//                .cors((Customizer<CorsConfigurer<HttpSecurity>>) corsConfig)   // todo cors
+                .build();
     }
 
     // 패스워드 암호화
@@ -31,6 +47,7 @@ public class WebSecurityConfig  {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 
 }
 
